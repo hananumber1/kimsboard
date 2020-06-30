6ec9f21bb9c08e43c8d0c320806f0c9f84454a34
Junki Kim <zxc7023@gmail.com>
2020-06-30 21:17:17 +0900

    프로젝트 생성

---
+++ src/main/java/com/kimscooperation/kimsboard/KimsboardApplication.java
@@ -0,0 +1,13 @@
+package com.kimscooperation.kimsboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimsboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KimsboardApplication.class, args);
+	}
+
+}
