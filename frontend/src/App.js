28c785823e4a2b956d6900d5b91f8257c836da6e
Junki Kim <zxc7023@gmail.com>
2020-07-20 16:36:46 +0900

    front 초기 설정

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+const React = require('react');
+const { Fragment } = require('react');
+const { Component } = React;
+
+const App = () => {
+    return (
+        <>
+            <div>탑메뉴가 존재할 위치입니다.</div>
+            {/* 작성할 컴포넌트들  */}
+        </>
+    )
+    
+}
