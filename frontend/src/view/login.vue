de61081a4432f3521a9da34ff16d6941af0b3887
hanaKim <hananumber1@nate.com>
2020-08-09 19:57:19 +0900

    [add] login

--- frontend/src/view/login.vue
+++ frontend/src/view/login.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="Login">
-      <div class="id">
-          <input type="text" id="login_id" v-model="id">
-          <label for="login_id">아이디</label>
-      </div>
-      <div class="psw">
-          <input type="text" id="login_psw" v-model="password">
-          <label for="login_psw">비밀번호</label>
-      </div>
-      <button type="button">로그인</button>
+    <div class="id">
+      <input type="text" id="login_id" v-model="id" />
+      <label for="login_id">아이디</label>
+    </div>
+    <div class="psw">
+      <input type="password" id="login_psw" v-model="password" />
+      <label for="login_psw">비밀번호</label>
+    </div>
+    <button type="button" @click="setLogin">로그인</button>
   </div>
 </template>
 
 <script>
-
+import axios from 'axios';
 export default {
   name: "Login",
    data: function() {
@@ -24,11 +24,20 @@ export default {
   },
   methods:{
     setLogin(){
-
-    }
+      axios.post('http://localhost:8080/v1/signin', {
+      userId: this.id,
+      password: this.password
+  })
+  .then(function (response) {
+    // document.cookie = `accessToken=${data.token}`
+    // axios.defaults.headers.common.Authorization = `jwt ${response.data.token}`
+    // localStorage.setItem('userInfo', JSON.stringify(data))
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
   }
-};
+}
 </script>
-
-<style>
-</style>
